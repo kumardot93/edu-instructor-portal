7d9b20d9a4bf46fbd58dc38a8cd0dadeee826259
aditya <kumar.93@iitj.ac.in>
2020-07-16 22:27:55 +0530

    45% opacity for side bar buttons that is not implemented

--- src/components/Body/Sidebar.js
+++ src/components/Body/Sidebar.js
@@ -2,7 +2,10 @@ import React from 'react';
 import styles from './css/Sidebar.module.css';
 function Sidebar(props) {
 	return (
-		<div className={[ 'bg-light ml-auto m-2 mb-4 d-flex flex-row', styles.sidebar ].join(' ')}>
+		<div
+			className={[ 'bg-light ml-auto m-2 mb-4 d-flex flex-row', styles.sidebar ].join(' ')}
+			style={{ opacity: '35%' }}
+		>
 			<button className={[ 'flex-fill btn btn-dark m-1', styles.btnFont ].join(' ')}>
 				<sub className={[ 'material-icons mr-2', styles.mi ].join(' ')}>account_balance_wallet</sub>Earnings
 			</button>
